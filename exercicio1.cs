2ead4fc543612f2199d1e555fc87306e37e0908f
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-19 14:11:36 -0400

    nome da varivavel errada na condição do while

--- exercicio1.cs
+++ exercicio1.cs
@@ -6,23 +6,22 @@ namespace exercicio1
         static void Main (string[] args)
         {
             //Declaração de Variáveis
-           int [] vetor = new int[30];
+           int[] vetor = new int[30];
            int cont_ent=0, cont_impr=1, vet=0;//variaveis para  percorrer o vetor e impressao de mensagem e definir o tamnho do vetor
         
-           Console.Write("!---------Estrutura WHILE---------!\n\n");//\n\n para pualr duas linhas
+           Console.Write("!---------Estrutura WHILE---------!\n\n");//\n\n para pular duas linhas
           
-           Console.Write("Entre como  tamnho do vetor: \n");
+           Console.Write("Entre como  tamanho do vetor: \n");
            vet = int.Parse(Console.ReadLine());
            Console.Write("O vetor tera: "+vet+" posições!\n");
 
            //Preenchendo o vetor
-           While(cont_vet<vet)
+           While(cont_ent<vet)
            {
-             Console.WriteLine("Entre como o "+cont_impr+"° valor do vetor");
+             Console.WriteLine("Entre com o "+cont_impr+"° valor do vetor");
              vetor[cont_ent] = int.Parse(Console.ReadLine());
              cont_ent++;
              cont_impr++;
-
            }
 
         }
