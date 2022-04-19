7f7ae0316b89f5b6caada0d509c3d366769e260c
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-19 11:49:18 -0400

    first commit

---
+++ exercicio1.cs
@@ -0,0 +1,11 @@
+using System;
+namespace exercicio1
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+           Console.Write("!---------Estrutura WHILE---------!") 
+        }
+    }
+}
