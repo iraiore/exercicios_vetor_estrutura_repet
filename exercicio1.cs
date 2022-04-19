dc124cb627f1b51a55977c9cde6565f14d2781a0
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-19 12:09:57 -0400

    att

--- exercicio1.cs
+++ exercicio1.cs
@@ -7,11 +7,13 @@ namespace exercicio1
         {
             //Declaração de Variáveis
            int [] vetor = new int[30];
-           int cont_entrada=0, cont_impr=0;//variaveis para  percorrer o vetor e impressao de mensagem
+           int cont_entrada=0, cont_impr=0, vet=0;//variaveis para  percorrer o vetor e impressao de mensagem e definir o tamnho do vetor
         
-           Console.Write("!---------Estrutura WHILE---------!\n\n");
-           Console.Write("--");
-           
+           Console.Write("!---------Estrutura WHILE---------!\n\n");//\n\n para pualr duas linhas
+           Console.Write("Entre como  tamnho do vetor: \n");
+           vet = int.Parse(Console.ReadLine());
+           Console.Write("O tamnho do vetor será: "+vet);
+
         }
     }
 }
