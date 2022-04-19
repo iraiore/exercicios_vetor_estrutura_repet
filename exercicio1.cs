83209965494ef9dc3ee6c6b955a7e216ef3057a5
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-19 15:57:34 -0400

    teste para impresssão do vetor em arvore com o laço for

--- exercicio1.cs
+++ exercicio1.cs
@@ -3,7 +3,7 @@ namespace exercicio1
 {
     public class Program
     {
-        static void Main (string[] args)
+        public static void Main (string[] args)
         {
             //Declaração de Variáveis
            int[] vetor = new int[30];
@@ -14,15 +14,24 @@ namespace exercicio1
            Console.Write("Entre com  tamanho do vetor: \n");
            vet = int.Parse(Console.ReadLine());
            Console.Write("O vetor tera: "+vet+" posições!\n");
-           Console.WriteLine("o valor das variaveis i e j são respectivamente: "+i+","+j);
+           Console.WriteLine("\no valor das variaveis i e j são respectivamente: "+i+","+j);
 
            //Preenchendo o vetor
            while (i<vet)
            {
-             Console.WriteLine("Entre com o "+j+"° valor do vetor");
+             Console.WriteLine("\nEntre com o "+j+"° valor do vetor");
              vetor[i] = int.Parse(Console.ReadLine());
              i++;
              j++;
+             
+             //imprimindo o vetor em arvore com o a estrutura for
+             Console.WriteLine("O vetor de "+vet+"posições é: ");
+            for(i=0; i++; i<vet)
+            {
+                Console.Write(vetor[i]);
+                Console.WriteLine("\n")
+            }
+             
            }
         }
     }
