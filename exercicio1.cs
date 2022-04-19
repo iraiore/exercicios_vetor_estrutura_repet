d790bc3a0bc59d13bbdf5c675b7264a649b79394
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-19 12:00:44 -0400

    declarando duas variaveis na mesma linha

--- exercicio1.cs
+++ exercicio1.cs
@@ -5,6 +5,10 @@ namespace exercicio1
     {
         static void Main (string[] args)
         {
+            //Declaração de Variáveis
+           int [] vetor = new int[30];
+           int cont_entrada=0, cont_impr=0;//variavem para  percorrer o vetor
+        
            Console.Write("!---------Estrutura WHILE---------!") 
         }
     }
