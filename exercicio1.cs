6ba39cafb331b86ad6c2a9605e672aedc4b1b07b
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-19 14:21:45 -0400

    ainda sem encontrar o erro

--- exercicio1.cs
+++ exercicio1.cs
@@ -7,21 +7,21 @@ namespace exercicio1
         {
             //Declaração de Variáveis
            int[] vetor = new int[30];
-           int cont_ent=0, cont_impr=1, vet=0;//variaveis para  percorrer o vetor e impressao de mensagem e definir o tamnho do vetor
+           int i=0, j=1, vet=0;//variaveis para  percorrer o vetor e impressao de mensagem e definir o tamnho do vetor
         
            Console.Write("!---------Estrutura WHILE---------!\n\n");//\n\n para pular duas linhas
           
-           Console.Write("Entre como  tamanho do vetor: \n");
+           Console.Write("Entre com  tamanho do vetor: \n");
            vet = int.Parse(Console.ReadLine());
            Console.Write("O vetor tera: "+vet+" posições!\n");
 
            //Preenchendo o vetor
-           While(cont_ent<vet)
+           While(i<vet)
            {
              Console.WriteLine("Entre com o "+cont_impr+"° valor do vetor");
-             vetor[cont_ent] = int.Parse(Console.ReadLine());
-             cont_ent++;
-             cont_impr++;
+             vetor[i] = int.Parse(Console.ReadLine());
+             i++;
+             j++;
            }
 
         }
