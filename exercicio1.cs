a3c01a549f6f43cc2cabc7e424e08a61362d0bca
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-24 13:16:49 -0400

    verificando se ha alguma coisa dentro dos contadores de a e b do 1º for

--- exercicio1.cs
+++ exercicio1.cs
@@ -80,6 +80,7 @@ namespace exercicio1
 
            //IMPRIMINDO O VETOR DE  FORMA DECRESCENTE COM A ESTRUTURA FOR
            Console.WriteLine("\nO vetor de "+vet+" posições é: \n");
+           Console.WriteLine(a+"e"+b);
            for(int a=vet-1; a>0; a--)
            {
 	            //Console.Write("\na= "+a+"\n");//entendendo o laço for para impressao no estilo crescente da variavel a
