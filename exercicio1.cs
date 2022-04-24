6bbab36dad383acb696ecb7d4db9829db9328c95
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-24 12:55:06 -0400

    testando o codigo ate a leitura dos dados da estrutura do...while

--- exercicio1.cs
+++ exercicio1.cs
@@ -28,9 +28,9 @@ namespace exercicio1
             }
 
             //IMPRIMINDO O VETOR DE  FORMA CRESCENTE COM A ESTRUTURA DO FOR
-             Console.WriteLine("\nO vetor de "+vet+" posições é: \n");
-             for(int a=0; a<vet; a++)
-             {
+           Console.WriteLine("\nO vetor de "+vet+" posições é: \n");
+           for(int a=0; a<vet; a++)
+           {
 	            //Console.Write("\na= "+a+"\n");//entendendo o laço for para impressao no estilo crescente da variavel a
                 for (int b=0;b<=a;b++)
                 {
@@ -39,20 +39,44 @@ namespace exercicio1
                 }
 		        Console.WriteLine("\n");//pular uma linha para cada impressão do vetor com o contador b
             }
-             //ENCONTRANDO E IMPRIMINDO OS VALORES MAIORES QUE 100
-             //inicializando os contadores novamente para percorrer o vetor e encontrar os valores maiores que 100 
-             i=0;
-             j=0;
-             Console.WriteLine("Os valores maiores que 100 são: \n");
-             while (i<vet)
-             {
-                 if(vetor[i]>100)
-                 {
-                     Console.Write(vetor[i]+",");
-                 }
-                 i++;
-             }
-             Console.ReadLine();
+
+            //ENCONTRANDO E IMPRIMINDO OS VALORES MAIORES QUE 100
+            //inicializando os contadores novamente para percorrer o vetor e encontrar os valores maiores que 100 
+            i=0;
+            j=0;
+            Console.WriteLine("Os valores maiores que 100 são: \n");
+            while (i<vet)
+            {
+                if(vetor[i]>100)
+                {
+                    Console.Write(vetor[i]+",");
+                }
+                i++;
+            }
+            Console.WriteLine("Pressione qualquer tecla para iniciar a estrutura do..while");
+            Console.ReadLine();
+
+            Console.Write("!---------Estrutura DO... WHILE---------!\n\n");//\n\n para pular duas linhas
+            //inicializando os vetores par aque seja utilzado na estrutura do...while
+            i=0;
+            j=1;
+
+           //bloco para entrada e leitura do usuário para determinar o tamanho do vetor - o exercicio pede um vetor de 30 posições
+           Console.Write("Entre com  tamanho do vetor: \n");
+           vet = int.Parse(Console.ReadLine());
+
+           //mensagem anunciando o tamnho do vetor e o vetor assumindo o numeor de posições
+           Console.Write("\nO vetor tera: "+vet+" posições!\n");
+           int[] vetor = new int[vet];
+
+           //PREENCHENDO O VETOR COM A ESTRUTURA DO... WHILE
+           do
+           {
+             Console.WriteLine("\nEntre com o "+j+"° valor do vetor");
+             vetor[i] = int.Parse(Console.ReadLine());
+             i++;
+             j++;
+           }while (i<vet)
         }
     }
 }
