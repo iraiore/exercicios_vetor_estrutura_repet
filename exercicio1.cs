507bfb11a83c4ad126acc0800a086cea24fee2d3
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-04-24 13:08:58 -0400

    imprimir o vetor em ordem descrescente

--- exercicio1.cs
+++ exercicio1.cs
@@ -14,7 +14,7 @@ namespace exercicio1
            Console.Write("Entre com  tamanho do vetor: \n");
            vet = int.Parse(Console.ReadLine());
 
-           // mensagem anunciando o tamnho do vetor e o vetor assumindo o numeor de posições
+           // mensagem anunciando o tamnho do vetor e o vetor assumindo o numero de posições
            Console.Write("\nO vetor tera: "+vet+" posições!\n");
            int[] vetor = new int[vet];
 
@@ -27,7 +27,7 @@ namespace exercicio1
              j++;             
             }
 
-            //IMPRIMINDO O VETOR DE  FORMA CRESCENTE COM A ESTRUTURA DO FOR
+            //IMPRIMINDO O VETOR DE  FORMA CRESCENTE COM A ESTRUTURA FOR
            Console.WriteLine("\nO vetor de "+vet+" posições é: \n");
            for(int a=0; a<vet; a++)
            {
@@ -53,7 +53,7 @@ namespace exercicio1
                 }
                 i++;
             }
-            Console.WriteLine("Pressione qualquer tecla para iniciar a estrutura do..while");
+            Console.WriteLine("\nPressione qualquer tecla para iniciar a estrutura do..while");
             Console.ReadLine();
 
             Console.Write("!---------Estrutura DO... WHILE---------!\n\n");//\n\n para pular duas linhas
@@ -65,7 +65,7 @@ namespace exercicio1
            Console.Write("Entre com  tamanho do vetor: \n");
            vet = int.Parse(Console.ReadLine());
 
-           //mensagem anunciando o tamnho do vetor e o vetor assumindo o numeor de posições
+           //mensagem anunciando o tamnho do vetor e o vetor assumindo o numero de posições
            Console.Write("\nO vetor tera: "+vet+" posições!\n");
            int[] vetorD = new int[vet];//necessario criar outro vetor pois, nao sei como reinicializar e reutilizar o mesmo vetor para estrutura do...while
 
@@ -73,10 +73,24 @@ namespace exercicio1
            do
            {
              Console.WriteLine("\nEntre com o "+j+"° valor do vetor");
-             vetor[i] = int.Parse(Console.ReadLine());
+             vetorD[i] = int.Parse(Console.ReadLine());
              i++;
              j++;
            }while (i<vet);
+
+           //IMPRIMINDO O VETOR DE  FORMA DECRESCENTE COM A ESTRUTURA FOR
+           Console.WriteLine("\nO vetor de "+vet+" posições é: \n");
+           for(int a=vet; a>0; a--)
+           {
+	            //Console.Write("\na= "+a+"\n");//entendendo o laço for para impressao no estilo crescente da variavel a
+                for (int b=a;b>0;b--)
+                {
+                    Console.Write(vetor[b]+" ");
+		            //Console.Write("b= "+b+" \n");//entendendo o laço for para impressao no estilo crescente variavel b - funçao aninhada
+                }
+		        Console.WriteLine("\n");//pular uma linha para cada impressão do vetor com o contador b
+                Console.ReadLine();
+            }
         }
     }
 }
