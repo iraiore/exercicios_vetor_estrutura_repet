39382a9854e872e74c1566b74c6e38c5e9c4a860
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-05-12 22:24:44 -0400

    att

--- exercicio4.cs
+++ exercicio4.cs
@@ -1,30 +1,30 @@
-    using System;
-    namespace exercicio4
+using System;
+namespace exercicio4
+{
+    public class Program 
     {
-        public class Program 
+        public static void Main (string[] args)
         {
-            public static void Main (string[] args)
-            {
-                int a = 0, b = 0, c = 1, soma=0, divisor=0;
-                float media = 0;
+            int a = 0, b = 0, c = 1, soma=0, divisor=0;
+            float media = 0;
             
-                while(a >= 0)
+            while(a >= 0)
+            {
+                Console.Write ("Entre com o "+c+"º numero: \n");
+                b = int.Parse(Console.ReadLine());
+                if (b < 0)
                 {
-                    Console.Write ("Entre com o "+c+"º numero: \n");
-                    b = int.Parse(Console.ReadLine());
-                    if (b < 0)
-                    {
-                        divisor = a;
-                        a = -1;
-                    }else
-                    {
-                    soma = soma + b;
-                    a++;c++;
-                    }
+                    divisor = a;
+                    a = -1;
+                }else
+                {
+                soma = soma + b;
+                a++;c++;
                 }
-                media = b / divisor;
-                Console.Write("a media dos valores é: "+ media);
-                Console.ReadLine();
             }
+            media = b / divisor;
+            Console.Write("a media dos valores é: "+ media);
+            Console.ReadLine();
         }
     }
+}
