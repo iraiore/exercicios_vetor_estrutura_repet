14dbf164ae8026a13e372710b963b5248eb30a5b
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-05-21 20:30:16 -0400

    reduzindo o numero de linhas

--- exercicio3.cs
+++ exercicio3.cs
@@ -5,38 +5,36 @@ namespace exercicio3
     {
         public static void Main (string[] args)
         {
-           Console.Write("!---------Estrutura WHILE---------!\n\n");//\n\n para pular duas linhas
-           int i=0, j=1, k=0, vet=0, somaVet=0, med;
+           Console.Write("!-----------------------ESTRUTURA WHILE-----------------------!\n\n");
+           int i=0, j=1, k=0, vet=0, somaVet=0, med;//declaração de variáveis
             //Defininado o tamnho do vetor
             Console.WriteLine("Entre com o tamnho do vetor: ");
             vet=int.Parse(Console.ReadLine());
-            int[] medVet = new int [vet];// vetor assumindo o tamanho entrado pelo usuário
-
-            //preenchendo o vetor
+            int[] medVet = new int [vet];
+            //mensagem alertando sobre a entrada zerada
             Console.WriteLine("                    ATENÇÃO!!!\n"+
             "Se o dado informado for igual a zero o vetor não sera mais preenchido,\n"+
             "mesmo que o valor total do vetor nao tenha sido ocpupado, neste caso, as\n"+
             "demais posições do vetor assumirão o valor zero!");
             //preechendo o vetor
-            while (i<vet)
+            while (i < vet)
             {
                 Console.WriteLine("\nEntre com o valor do "+j+"º vetor: ");
                 medVet[i]=int.Parse(Console.ReadLine());
-               //alocando zero na posição i ate o fim do vetor caso o valor digitado seja zero
-                if(medVet[i]==0)
+               //verificando se o valor é zero e zerando o restante do vetor
+                if(medVet[i] == 0)
                 {
-                    //estrutura para percorrer o vetor e zerar as demais posições
                     k=i;
-                    while(k<vet)
+                    while(k < vet)
                     {
                         medVet[k]=0;
                         k++;
                     }
-                    //estrutura para contar quantos valores são maiores que zero e somar os dados do vetor
+                    //contando os  valores maiores que zero e somando os dados do vetor
                     k=0;
-                    while(medVet[k]>0)
+                    while(medVet[k] > 0)
                     {
-                        somaVet=somaVet+medVet[k];
+                        somaVet = somaVet + medVet[k];
                         k++;
                     }
                     i=vet;
@@ -48,8 +46,8 @@ namespace exercicio3
             }
             //imprimindo o vetor
             i=0;
-            Console.Write("\nO vetor é: ");
-            while(i<vet)
+            Console.Write("\nOs valores do vetor são: ");
+            while(i < vet)
             {
             Console.Write(medVet[i]+",");
             i++;            
